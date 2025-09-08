60cce2e59c0366f239ace8051eec42650e9f2205
James <healyjames97@gmail.com>
2025-09-08 16:21:24 +0100

    replace tailwind with vanilla css

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,3 @@
-// @ts-check
-
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
@@ -8,7 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
+      plugins: [],
 	},
 
   integrations: [react()],
