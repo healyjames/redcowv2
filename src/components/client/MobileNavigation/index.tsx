cc77b07ce34d4c2ec6516bd19003755bcc523d59
James <healyjames97@gmail.com>
2025-09-17 12:16:26 +0100

    mobile nav final touches

--- src/components/client/MobileNavigation/index.tsx
+++ src/components/client/MobileNavigation/index.tsx
@@ -25,20 +25,35 @@ export default function MobileNav() {
           </div>
 
           {open && (
-            <ul className="mobile-nav-list">
-                <li className="mobile-nav-item">
-                    <a href="/dine">Dine</a>
-                </li>
-                <li className="mobile-nav-item">
-                    <a href="/stay">Stay</a>
-                </li>
-                <li className="mobile-nav-item">
-                    <a href="/events">Events</a>
-                </li>
-                <li className="mobile-nav-item">
-                    <a href="/contact">Contact</a>
-                </li>
-            </ul>
+              <div className="mobile-nav-content">
+                  <ul className="mobile-nav-list">
+                      <li className="mobile-nav-item">
+                          <a href="/dine">Dine</a>
+                      </li>
+                      <li className="mobile-nav-item">
+                          <a href="/stay">Stay</a>
+                      </li>
+                      <li className="mobile-nav-item">
+                          <a href="/events">Events</a>
+                      </li>
+                      <li className="mobile-nav-item">
+                          <a href="/contact">Contact</a>
+                      </li>
+                  </ul>
+                  <div className="mobile-nav-footer">
+                      <ul className="mobile-nav-footer-top">
+                          <li>
+                              <a href="/about">About</a>
+                          </li>
+                          <li>
+                              <a href="/contact">Contact</a>
+                          </li>
+                      </ul>
+                      <ul className="mobile-nav-footer-bottom">
+                          <p>Follow us on <a href="https://www.facebook.com/theredcow" target="_blank" rel="noopener noreferrer">Facebook</a>, <a href="https://www.instagram.com/theredcow" target="_blank" rel="noopener noreferrer">Instagram</a> &amp; <a href="https://www.tiktok.com/@theredcow" target="_blank" rel="noopener noreferrer">TikTok</a></p>
+                      </ul>
+                  </div>
+              </div>
           )}
       </nav>
   );
