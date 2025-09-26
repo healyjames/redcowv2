c12d90f8554212a1aa4a2a74ff4a52211e7c1a15
James <healyjames97@gmail.com>
2025-09-26 20:35:54 +0100

    sitemap and robots txt

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  vite: {
-      plugins: [],
-	},
-
-  integrations: [react()],
+    vite: {
+        plugins: [],
+    },
+    site: "https://example.com",
+    integrations: [react(), sitemap()],
 });
