ffd30add9f3844a82df75906e080a5a53e5eb908
James <healyjames97@gmail.com>
2025-10-12 16:04:36 +0100

    remove unused env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,6 @@
 
 interface ImportMetaEnv {
     readonly RESEND_API_KEY: string;
-    readonly SMTP_HOST?: string;
-    readonly SMTP_PORT?: string;
-    readonly SMTP_USER?: string;
-    readonly SMTP_PASS?: string;
-    readonly SENDGRID_API_KEY?: string;
 }
 
 interface ImportMeta {
