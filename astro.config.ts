95b17ebd6b760d01d89f979f8ac4919ab4225203
James <healyjames97@gmail.com>
2025-10-12 16:51:28 +0100

    netlify adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import path from "node:path";
 
 export default defineConfig({
-    vite: {
-        plugins: [],
-        resolve: {
-            alias: {
-                "@": path.resolve("./src"),
-            },
-        },
-    },
-    site: "https://example.com",
-    integrations: [react(), sitemap()],
-    output: "static",
+  vite: {
+      plugins: [],
+      resolve: {
+          alias: {
+              "@": path.resolve("./src"),
+          },
+      },
+  },
+
+  site: "https://example.com",
+  integrations: [react(), sitemap()],
+  output: "static",
+  adapter: netlify(),
 });
