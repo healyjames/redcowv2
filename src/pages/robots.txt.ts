725b615e2346f27695e236c747ae4d7c82016984
James <healyjames97@gmail.com>
2026-02-17 17:55:27 +0000

    sitemap updates 2

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -10,6 +10,6 @@ Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-    const sitemapURL = new URL("sitemap-0.xml", site);
+    const sitemapURL = new URL("sitemap-index.xml", site);
     return new Response(getRobotsTxt(sitemapURL));
 };
